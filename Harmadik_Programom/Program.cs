7a4d94a98bfd0aa8c0b42c4ad5a52b0398474b3e
JR Force <87035767+JRForce261@users.noreply.github.com>
2022-02-03 21:32:59 +0100

    Add files via upload

---
+++ Harmadik_Programom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmadik_Programom
+{
+    class Program
+    {
+        static void Main()
+        {
+            string keresztnev = "";
+            string vezeteknev = "";
+            string monogram = "";
+
+            Console.Write("Kérem a Vezetéknevét: ");
+            vezeteknev = Console.ReadLine();
+            Console.Write("Kérem a Keresztnevét: ");
+            keresztnev = Console.ReadLine();
+
+            monogram = vezeteknev[0].ToString();
+            monogram += keresztnev[0].ToString();
+
+            Console.Write("A monogram: ");
+            Console.WriteLine(monogram);
+            Console.ReadKey();
+        }
+    }
+}
