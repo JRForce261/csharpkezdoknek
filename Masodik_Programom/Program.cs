184a09b26f7f2c981005f9fc8c0c698b999c0d79
JR Force <87035767+JRForce261@users.noreply.github.com>
2022-02-03 21:32:36 +0100

    Add files via upload

---
+++ Masodik_Programom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masodik_Programom
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sum = 0;
+            double atlag = 0.00;
+            double szorzata = 1;
+            for (int i = 0; i <= 10; i++)
+            {
+                sum += i;
+            }
+            atlag = sum / 10;
+            int j = 1;
+            while(j<=10)
+            {
+                szorzata = szorzata * j;
+                j++;
+            }    
+            Console.WriteLine("Számok öszege:");
+            Console.WriteLine(sum);
+            Console.WriteLine("Számok átlaga:");
+            Console.WriteLine(atlag);
+            Console.WriteLine("Számok szorzata:");
+            Console.WriteLine(szorzata);
+            Console.ReadKey();
+        }
+    }
+}
